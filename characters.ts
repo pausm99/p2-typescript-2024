f5ed00281c2a0f2a04138d03a2c842fb0bedf021
pausm99 <pausm99@gmail.com>
2024-04-08 19:19:07 +0200

    refactorized fetchCaracters to fetch all pages

--- characters.ts
+++ characters.ts
@@ -20,8 +20,38 @@ export class Character {
 
 const apiUrl = "https://rickandmortyapi.com/api/character";
 
-export const fetchCharacters = async (pageNumber: number) => {
+const fetchCharacters = async (pageNumber: number) => {
     const response = await fetch(`${apiUrl}?page=${pageNumber}`);
     const { results, info } = (await response.json()) as { results: Character[], info: any };
-    return results;
+    return { results, info };
+}
+
+interface Info {
+    count: number;
+    pages: number;
+    next: string;
+    prev: string;
+}
+
+export const getCharacters = async () => {
+    let currentPage = 1;
+
+    const { results, info } = await fetchCharacters(currentPage);
+    let characters: Character[] = results;
+    let pageInfo: Info = info;
+
+    ++currentPage;
+
+    while(currentPage < pageInfo.pages) {
+        const { results, info } = await fetchCharacters(currentPage);
+        
+        characters = characters.concat(results);
+        pageInfo = info;
+        
+        if (pageInfo.next === null) break;
+
+        ++currentPage;
+    };
+
+    return characters;
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { Character, fetchCharacters } from "./characters";
+import { Character, getCharacters } from "./characters";
 import { render } from './render';
 import { writeFile } from "fs/promises";
 
-const characters: Character[] = await fetchCharacters(1);
+const characters: Character[] = await getCharacters();
 const html = render(characters);
 await writeFile('characters.html', html);
