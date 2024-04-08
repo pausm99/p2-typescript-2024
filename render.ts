222570ac14ae3b4b827791a027699967d3837991
pausm99 <pausm99@gmail.com>
2024-04-08 19:40:12 +0200

    load styles with separate file

--- render.ts
+++ render.ts
@@ -1,4 +1,5 @@
 import type { Character } from "./characters";
+import { readFileSync } from "fs";
 
 const head = (title: string) => {
     return `
@@ -6,24 +7,35 @@ const head = (title: string) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${title}</title>
+        <style>
+            ${loadStyles()}
+        </style>
     `;
 };
 
 const renderCharacters = (characters: Character[]) => {
     let html = "";
     if (characters.length > 0) {
-        html += `<ul></ul>`;
+        html += `<ul>`;
+        
+        characters.forEach(character => {
+            html += `
+                <li class="character">
+                    <h2>${character.name}</h2>
+                    <img src="${character.image}" alt="${character.name} photo">
+                </li>
+            `;
+        });
+    
+        html += '</ul>';
     }
-    characters.forEach(character => {
-        html += `
-            <li class="character">
-                <h2>${character.name}</h2>
-                <img src="${character.image}" alt="${character.name} photo">
-            </li>
-        `;
-    });
 
     return html;
+}
+
+const loadStyles = () => {
+    const styles = readFileSync('./styles.css', 'utf8');
+    return styles;
 }
 
 export const render = (characters: Character[]) => {
